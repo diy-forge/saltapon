59a2327b62fa460a984f8b58e8f457ba28f5ba82
Manuel Eusebio de Paz Carmona <pelirrojo@users.noreply.github.com>
2021-02-07 14:29:41 +0100

    Env setup

---
+++ conda_setup.sh
@@ -0,0 +1,8 @@
+
+wget https://github.com/jjhelmus/berryconda/releases/download/v2.0.0/Berryconda3-2.0.0-Linux-armv6l.sh
+
+conda create -n py36 python=3.6
+conda create -n py37 python=3.7
+conda create -n py38 python=3.8
+
+
